3cc9870c8bd90228e909fb586309f802f5bb2f4a
JW0203 <widwoo0203@gmail.com>
2024-05-21 12:31:53 +0900

    feat: set the typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'nestjs_jwt',
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
