ad0b8cd07cb4d495cdf08cae0ea8755ce93a25ae
JW0203 <widwoo0203@gmail.com>
2024-05-22 16:02:51 +0900

    feat: implement Sign-in service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthService } from '../auth/auth.service';
+import { SignInRequestDto } from './dto/signIn.request.dto';
 
 @Injectable()
 export class UserService {
@@ -11,4 +12,10 @@ export class UserService {
     private userRepository: Repository<User>,
     private readonly authService: AuthService,
   ) {}
+
+  async signIn(signInRequestDto: SignInRequestDto): Promise<User> {
+    const { email, password } = signInRequestDto;
+    const newUSer = new User({ email, password });
+    return this.userRepository.save(newUSer);
+  }
 }
