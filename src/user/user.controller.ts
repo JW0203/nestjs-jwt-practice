fa1ec8d8d5e4b42fd535c8c9db0ee62b64b00fac
JW0203 <widwoo0203@gmail.com>
2024-05-22 16:04:15 +0900

    feat: implement Sign-in service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { SignInRequestDto } from './dto/signIn.request.dto';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  signIn(signInRequestDto: SignInRequestDto) {
+    return this.userService.signIn(signInRequestDto);
+  }
 }
