30a803b87f220eeba4eb0a17ff545bcf0987a27f
JW0203 <widwoo0203@gmail.com>
2024-05-25 08:55:32 +0900

    feat : implementing protected route and JWT strategy guards

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,17 +5,19 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'jwt_secret',
+      secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
     forwardRef(() => UserModule),
     PassportModule,
   ],
-  providers: [AuthService, LocalStrategy],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
   controllers: [AuthController],
   exports: [AuthService],
 })
