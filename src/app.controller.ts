1791a64ba2555c829ef2b00c869a634a6c5935ec
JW0203 <widwoo0203@gmail.com>
2024-05-25 02:17:51 +0900

    feat : implementing login route with Guards

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,24 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  // @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    /*
+    이 라우트를 사용할 때  body 에
+    {
+      "username" : "test@test.com",
+      "password" : "12345678"
+    }
+    이렇게 주어야 정상적인 작동을 하여 아래를 반환
+
+    "id": 6,
+    "email": "test@test.com",
+    "createdAt": "2024-05-24T09:35:14.740Z"
+
+    */
+    return req.user;
   }
 }
