5a7df5ea4fd4a80e25f48233fac92425fc2e8a38
JW0203 <widwoo0203@gmail.com>
2024-05-24 18:45:01 +0900

    feat: add api to chcking payload

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  checkPayload() {
-    return this.authService.signWithJwt({ id: 1 });
+  @Get(':id')
+  checkPayload(@Param('id') id: string) {
+    return this.authService.signWithJwt({ id: id });
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { AuthService } from './auth.service';
     }),
   ],
   providers: [AuthService],
+  controllers: [AuthController],
   exports: [AuthService],
 })
 export class AuthModule {}
