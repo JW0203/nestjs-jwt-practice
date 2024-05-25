8896149f72e921717e0c407049d7b45c068db98d
JW0203 <widwoo0203@gmail.com>
2024-05-25 16:20:00 +0900

    fix: change jwt expire time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { JwtStrategy } from './jwt.strategy';
   imports: [
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '1m' },
     }),
     forwardRef(() => UserModule),
     PassportModule,
