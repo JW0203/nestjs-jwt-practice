2d1724d48562ae176654c99a1ff36399b0e35853
JW0203 <widwoo0203@gmail.com>
2024-05-25 10:16:04 +0900

    add explanation

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -12,7 +12,10 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       secretOrKey: jwtConstants.secret,
     });
   }
-
+  /*
+  포스트맨 헤더에 signIn 으로 발급받은 accessToken 입력하면
+  jwt 토큰에 담겨있는 id 와 email
+   */
   async validate(payload: any): Promise<any> {
     return { id: payload.id, email: payload.email };
   }
