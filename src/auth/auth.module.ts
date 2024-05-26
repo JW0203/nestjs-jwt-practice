66543a5b651ef696891a3ee57d999ad942b2d89a
JW0203 <widwoo0203@gmail.com>
2024-05-27 03:15:39 +0900

    feat: store configuration variables in .env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
@@ -5,14 +7,24 @@ import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { PassportModule } from '@nestjs/passport';
 import { LocalStrategy } from './local.strategy';
-import { jwtConstants } from './constants';
+// import { jwtConstants } from './constants';
+// import * as process from 'node:process';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '3m' },
+    // JwtModule.register({
+    //   // secret: jwtConstants.secret,
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '3m' },
+    // }),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '3m' },
+      }),
     }),
     forwardRef(() => UserModule),
     PassportModule,
--- src/auth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-  secret: 'KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
-};
