8dc988b376115f24e8f1b34f3881706effe9f47c
JW0203 <widwoo0203@gmail.com>
2024-05-28 12:01:47 +0900

    fix: change the name of key

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -2,16 +2,15 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
-import { UserService } from '../user/user.service';
 import { ValidateUserRequestDto } from './dto/validateUser.request.dto';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly userService: UserService,
-  ) {
-    super();
+  constructor(private readonly authService: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
   }
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(new ValidateUserRequestDto({ email, password }));
