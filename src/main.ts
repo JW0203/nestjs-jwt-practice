67e0c79e59ca8fe44063d840b9c2efcb232daf82
JW0203 <widwoo0203@gmail.com>
2024-06-02 16:59:34 +0900

    feat: get the info of port and host-ip from .env file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(configService.get<number>('PORT'), configService.get<string>('HOST_IP'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
